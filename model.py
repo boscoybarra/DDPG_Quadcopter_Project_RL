8e7fa2b4456ef2f34b6fbe1eb883c0793e2dd667
boscoybarra <bosco@ybarra.io>
2018-05-04 20:46:54 +0200

    creating the model to improve rewards critic and actor

---
+++ model.py
@@ -0,0 +1,178 @@
+from keras import layers, models, optimizers
+from keras import backend as K
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Flatten, Dropout
+from keras.layers.normalization import BatchNormalization
+from keras import regularizers
+import numpy as np
+
+# Neural network architectures for our model.
+
+class Actor:
+    """Actor (Policy) Model."""
+
+    def __init__(self, state_size, action_size, action_low, action_high, action_range):
+        """Initialize parameters and build model.
+
+        Params
+        ======
+            state_size (int): Dimension of each state
+            action_size (int): Dimension of each action
+            action_low (array): Min value of each action dimension
+            action_high (array): Max value of each action dimension
+            action_range (array): Range value of each action dimension
+        """
+        self.state_size = state_size
+        self.action_size = action_size
+        self.action_low = action_low
+        self.action_high = action_high
+        self.action_range = self.action_high - self.action_low
+
+        self.w = np.random.normal(
+            size=(self.state_size, self.action_size),  # weights for simple linear policy: state_space x action_space
+            scale=(self.action_range / (2 * self.state_size))) # start producing actions in a decent range
+
+        # Initialize any other variables here
+
+        self.build_model()
+
+    def build_model(self):
+        """Build an actor (policy) network that maps states -> actions."""
+        # Define input layer (states)
+        states = layers.Input(shape=(self.state_size,), name='states')
+
+        # actor = Sequential()
+
+        # # we can think of this chunk as the input layer
+        # actor.add(Dense(64, input_dim=14, init='uniform'))
+        # actor.add(BatchNormalization())
+        # actor.add(Activation('tanh'))
+        # actor.add(Dropout(0.5))
+
+        # # we can think of this chunk as the hidden layer with a regularizers
+        # actor.add(Dense(64, init='uniform'))
+        # actor.add(Dense(64, input_dim=64,
+        #         kernel_regularizer=regularizers.l2(0.01),
+        #         activity_regularizer=regularizers.l1(0.01)))
+        # actor.add(BatchNormalization())
+        # actor.add(Activation('tanh'))
+        # actor.add(Dropout(0.5))
+
+        # # we can think of this chunk as the output layer
+        # actor.add(Dense(2, init='uniform'))
+        # actor.add(BatchNormalization())
+        # actor.add(Activation('softmax'))
+        # print(actor.summary())
+
+        # Add hidden layers
+        net = layers.Dense(units=32, activation='relu')(states)
+        net = layers.Dense(units=64, activation='relu')(net)
+        net = layers.Dense(units=32, activation='relu')(net)
+
+        # Try different layer sizes, activations, add batch normalization, regularizers, etc.
+        # instantiate model
+
+
+        # Add final output layer with sigmoid activation
+        raw_actions = layers.Dense(units=self.action_size, activation='sigmoid',
+            name='raw_actions')(net)
+
+        # Scale [0, 1] output for each action dimension to proper range
+        actions = layers.Lambda(lambda x: (x * self.action_range) + self.action_low,
+            name='actions')(raw_actions)
+
+        # Create Keras model
+        self.model = models.Model(inputs=states, outputs=actions)
+
+        # Define loss function using action value (Q value) gradients
+        action_gradients = layers.Input(shape=(self.action_size,))
+        loss = K.mean(-action_gradients * actions)
+
+        # Incorporate any additional losses here (e.g. from regularizers)
+
+        # Define optimizer and training function
+        optimizer = optimizers.Adam()
+        updates_op = optimizer.get_updates(params=self.model.trainable_weights, loss=loss)
+        self.train_fn = K.function(
+            inputs=[self.model.input, action_gradients, K.learning_phase()],
+            outputs=[],
+            updates=updates_op)
+
+class Critic:
+    """Critic (Value) Model."""
+
+    def __init__(self, state_size, action_size):
+        """Initialize parameters and build model.
+
+        Params
+        ======
+            state_size (int): Dimension of each state
+            action_size (int): Dimension of each action
+        """
+        self.state_size = state_size
+        self.action_size = action_size
+
+        # Initialize any other variables here
+
+        self.build_model()
+
+    def build_model(self):
+        """Build a critic (value) network that maps (state, action) pairs -> Q-values."""
+        # Define input layers
+        states = layers.Input(shape=(self.state_size,), name='states')
+        actions = layers.Input(shape=(self.action_size,), name='actions')
+
+        # Add hidden layer(s) for state pathway
+        net_states = layers.Dense(units=32, activation='relu')(states)
+        net_states = layers.Dense(units=64, activation='relu')(net_states)
+
+        # Add hidden layer(s) for action pathway
+        net_actions = layers.Dense(units=32, activation='relu')(actions)
+        net_actions = layers.Dense(units=64, activation='relu')(net_actions)
+
+        # Try different layer sizes, activations, add batch normalization, regularizers, etc.
+
+        # Combine state and action pathways
+        net = layers.Add()([net_states, net_actions])
+        net = layers.Activation('relu')(net)
+
+        # Add more layers to the combined network if needed
+
+        # we can think of this chunk as the input layer
+        # net = layers.Dense(64, input_dim=14, init='uniform')(net)
+        # net = layers.normalization.BatchNormalization()(net)
+        # net = layers.Dense(64, input_dim=14, init='uniform')(net)
+        # net = layers.Activation('tanh')(net)
+        # net = layers.Dropout(0.5)(net)
+
+        # we can think of this chunk as the hidden layer with a regularizers
+        # net = critic.add(Dense(64, init='uniform')(net))
+        # net = critic.add(Dense(64, input_dim=64,
+        #         kernel_regularizer=regularizers.l2(0.01),
+        #         activity_regularizer=regularizers.l1(0.01))(net))
+        # net = critic.add(BatchNormalization()(net))
+        # net = critic.add(Activation('tanh')(net))
+        # net = critic.add(Dropout(0.5)(net))
+
+        # # we can think of this chunk as the output layer
+        # net = critic.add(Dense(2, init='uniform')(net))
+        # net = critic.add(BatchNormalization()(net))
+        # net = critic.add(Activation('softmax')(net))
+
+        # Add final output layer to prduce action values (Q values)
+        Q_values = layers.Dense(units=1, name='q_values')(net)
+
+        # Create Keras model
+        self.model = models.Model(inputs=[states, actions], outputs=Q_values)
+
+        # Define optimizer and compile model for training with built-in loss function
+        optimizer = optimizers.Adam()
+        self.model.compile(optimizer=optimizer, loss='mse')
+
+        # Compute action gradients (derivative of Q values w.r.t. to actions)
+        action_gradients = K.gradients(Q_values, actions)
+
+        # Define an additional function to fetch action gradients (to be used by actor model)
+        self.get_action_gradients = K.function(
+            inputs=[*self.model.input, K.learning_phase()],
+            outputs=action_gradients)
